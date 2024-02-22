0e8fc1273e1f5ab5db3d3ee40fcf40e3e3cd42d5
BraideyH <braideyhooper@gmail.com>
2024-02-21 22:38:29 -0600

    created the methods that will fetch user text input for specific hour blocks saved in local storage. This allows data to persist on reload.

--- Develop/script.js
+++ Develop/script.js
@@ -16,13 +16,48 @@ $('.saveBtn').on('click', function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+function changeColor() {
+  var currentTime = now.hours();
 
+  $('.time-block').each(function () {
+    var timeId = parseInt($(this).attr('id').split("hour")[1]);
+
+    if (timeId < currentTime) {
+      $(this).addClass('past');
+    }
+    else if (timeId === currentTime) {
+      $(this).removeClass('past');
+      $(this).removeClass('future');
+      $(this).addClass('present');
+    }
+    else {
+      $(this).removeClass('past');
+      $(this).removeClass('future');
+      $(this).addClass('present');
+    }
+  });
+}
+
+changeColor();
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
+  var userNotes = localStorage.getItem(timeBlock.id);
+    if (userNotes !== null) {
+      timeBlock.querySelector('textarea').value = userNotes;
+    }
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
